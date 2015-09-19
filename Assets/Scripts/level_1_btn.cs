be6e6388ae37d123b11b0f104f72ae12f84d14a8
MaschiniDev <maschinigames@gmail.com>
2015-09-19 21:09:12 +0200

    theCube_jhNORD_build_1
    Erste vollständige Version!

--- Assets/Scripts/level_1_btn.cs
+++ Assets/Scripts/level_1_btn.cs
@@ -4,17 +4,9 @@ using System.Collections;
 public class level_1_btn : MonoBehaviour
 {
     void OnMouseDown()
-    {
-        bool open = false;
-        
-        if (open == true) {
-            transform.localScale += new Vector3(-100, -100, -10);
-            open = false;
-        }   
-        else if(open == false) {
-            transform.localScale += new Vector3(100, 100, 10);
-            open = true;
-        }
-  
+    {       
+        transform.localScale += new Vector3(100, 100, 10);
+        Application.LoadLevel("TheCube");
+        Cursor.visible = false;
     }
 }
