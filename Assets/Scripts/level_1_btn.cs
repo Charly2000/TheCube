8672b01b2945b7ec60673e9694a6d3257bf3c406
MaschiniDev <maschinigames@gmail.com>
2015-09-19 16:02:07 +0200

    menu_1
    menü 0.5

---
+++ Assets/Scripts/level_1_btn.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level_1_btn : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        print(gameObject.name);
+        transform.localScale += new Vector3(2, 2, 2);
+
+    }
+
+//    public GameObject thelight = null;
+
+//    private void
+}
---
+++ Assets/Scripts/level_choose.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level_choose : MonoBehaviour {
+
+	// Use this for initialization
+    void Start() {
+
+        
+    }
+    Ray ray;
+    RaycastHit hit;
+	// Update is called once per frame
+	void Update () {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            
+        }
+	}
+
+}
